5974d9d47e5610b582626529473612b149473047
David Gregory <davidgregory084@gmail.com>
2020-12-01 22:14:33 +0000

    Replace decrypting transformation with mapping from SFS to PhysFS code

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,11 @@ repositories {
 
 version = "0.1.0-SNAPSHOT"
 
+dependencies {
+    implementation("org.ow2.asm:asm-commons:9.0")
+    implementation("org.ow2.asm:asm-util:9.0")
+}
+
 tasks.compileJava {
     sourceCompatibility = "1.8"
     targetCompatibility = "1.8"
@@ -16,11 +21,15 @@ tasks.compileJava {
 tasks.jar {
     archiveFileName.set("${project.name}.jar")
 
+    from(configurations.runtimeClasspath.get().map {
+        if (it.isDirectory) it else zipTree(it)
+    })
+
     manifest {
         attributes(mapOf(
-                "Agent-Class" to "ClassLoadAgent",
-                "Premain-Class" to "ClassLoadAgent",
-                "Implementation-Title" to "XorTransformer",
+                "Agent-Class" to "com.maddox.instrument.ClassLoadAgent",
+                "Premain-Class" to "com.maddox.instrument.ClassLoadAgent",
+                "Implementation-Title" to "SFSTransformer",
                 "Implementation-Version" to rootProject.version
         ))
     }
--- src/main/java/com/maddox/instrument/ClassLoadAgent.java
+++ src/main/java/com/maddox/instrument/ClassLoadAgent.java
@@ -1,10 +1,12 @@
+package com.maddox.instrument;
+
 import java.lang.instrument.Instrumentation;
 
 public class ClassLoadAgent {
     public static void agentmain(String agentArgs, Instrumentation inst) {
-        inst.addTransformer(new XorTransformer());
+        inst.addTransformer(new SFSTransformer());
     }
     public static void premain(String agentArgs, Instrumentation inst) {
-        inst.addTransformer(new XorTransformer());
+        inst.addTransformer(new SFSTransformer());
     }
 }
